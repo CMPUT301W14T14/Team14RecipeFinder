a2f510f47f9be299c80654fd775d31b9fe287d15
XUPINGI <xuping@ualberta.ca>
2014-02-11 19:53:18 -0700

    skeleton of comment and commentlist

---
+++ src/com/example/projectapp/Comment.java
@@ -0,0 +1,85 @@
+package com.example.projectapp;
+
+import java.util.Date;
+
+import android.graphics.Bitmap;
+import android.location.Location;
+
+
+public class Comment{
+	//Attributes
+	private String text=null;
+	private Long timePosted=null;
+	private Bitmap picture=null;
+	private Location location=null;
+	private String userName=null;
+	private CommentList childReply=null;
+	
+	//Methods:
+	
+	//Constructors:
+	
+	public Comment(String text,String userName,CommentList childReply){
+		this.text=text;
+		this.userName=userName;
+		this.timePosted=(new Date()).getTime();
+		//this.location=getCurrentLocation;
+		this.childReply=childReply;
+		this.childReply.setParent(this);
+	}
+	
+	public Comment(String text,String userName,CommentList childReply,Bitmap picture){
+		this.text=text;
+		this.userName=userName;
+		this.timePosted=(new Date()).getTime();
+		//this.location=getCurrentLocation;
+		this.childReply=childReply;
+		this.childReply.setParent(this);
+		this.picture=picture;
+	}
+	
+	//Setters&Getters:
+	
+	//For text
+	public void setText(String newText){
+		this.text=newText;
+	}
+	
+	public String getText(){
+		return this.text;
+	}
+	
+	//For picture
+	public Bitmap getPicture(){
+		return this.picture;
+	}
+	
+	//For time posted
+	public Long getTimePosted(){
+		return this.timePosted;
+	}
+	
+	//For Location
+	public void setLocation(Location newLocation){
+		this.location=newLocation;
+	}
+	
+	public Location getLocation(){
+		return this.location;
+	}
+	
+	//Operations:
+	
+	public void addReply(Comment newReply){
+		this.childReply.addReply(newReply);
+	}
+	
+	public boolean checkUserName(String userName){
+		if((this.userName).equals(userName)){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+}
---
+++ src/com/example/projectapp/CommentList.java
@@ -0,0 +1,76 @@
+package com.example.projectapp;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+
+public class CommentList{
+	//Sub Classes (just skeleton):
+	class TimePostedComparator implements Comparator<Comment>{
+
+		@Override
+		public int compare(Comment lhs, Comment rhs)
+		{
+
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
+	}
+	
+	class LocationComparator implements Comparator<Comment>{
+
+		@Override
+		public int compare(Comment lhs, Comment rhs)
+		{
+
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
+	}
+	
+	class PictureComparator implements Comparator<Comment>{
+
+		@Override
+		public int compare(Comment lhs, Comment rhs)
+		{
+
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		
+	}
+	
+	//Attributes:
+	ArrayList<Comment> comments=null;
+	Comment parent=null;
+	
+	//Methods:
+	
+	//Constructors:
+	public CommentList(ArrayList<Comment> comments){
+		this.comments=comments;
+	}
+	
+	//Getters&Setters:
+	
+	//Parent Comment
+	public void setParent(Comment parent){
+		this.parent=parent;
+	}
+	
+	public Comment getParent(){
+		return this.parent;
+	}
+	
+	//Operations:
+	public void addReply(Comment reply){
+		comments.add(reply);
+	}
+	
+	//public Comment getOneComment(){
+	//}
+	
+	
+}
