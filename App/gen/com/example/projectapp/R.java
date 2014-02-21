17ecbaba76f18776ece89cc0256e16ba38c5a205
XUPINGI <xuping@ualberta.ca>
2014-02-21 16:36:10 -0700

    wrote skeleton of the loginActivity

--- App/gen/com/example/projectapp/R.java
+++ App/gen/com/example/projectapp/R.java
@@ -37,13 +37,13 @@ public final class R {
         public static final int greatnewsimage=0x7f080013;
         public static final int historytopic=0x7f080017;
         public static final int listView1=0x7f080003;
-        public static final int login_div=0x7f08001a;
-        public static final int login_user_input=0x7f08001b;
+        public static final int login_button=0x7f08001d;
+        public static final int login_layout=0x7f08001a;
+        public static final int login_userName_input_ins=0x7f08001b;
         public static final int logout=0x7f080019;
         public static final int reply=0x7f080005;
         public static final int restname=0x7f08000c;
         public static final int setname=0x7f080015;
-        public static final int signin_button=0x7f08001d;
         public static final int sort=0x7f080001;
         public static final int sortbylocation=0x7f08000d;
         public static final int sortbyotherlocation=0x7f08000f;
@@ -52,9 +52,9 @@ public final class R {
         public static final int topiclist=0x7f080011;
         public static final int tpecontent=0x7f080008;
         public static final int typename=0x7f08000b;
+        public static final int user_name_input=0x7f08001c;
         public static final int userinfo=0x7f080000;
         public static final int username=0x7f080016;
-        public static final int username_edit=0x7f08001c;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -65,7 +65,7 @@ public final class R {
         public static final int sortoption=0x7f030005;
         public static final int topicpage=0x7f030006;
         public static final int userinfopage=0x7f030007;
-        public static final int welcomepage=0x7f030008;
+        public static final int welcome_page=0x7f030008;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -74,6 +74,9 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int login_ins=0x7f050003;
+        public static final int typeUserName_ins=0x7f050005;
+        public static final int userName_ins=0x7f050004;
     }
     public static final class style {
         /** 
---
+++ App/src/com/example/projectapp/LoginViewActivity.java
@@ -0,0 +1,54 @@
+package com.example.projectapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginViewActivity extends Activity{
+	//Attributes:
+	private EditText userNameInput=null;
+	private Button loginButton=null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.welcome_page);
+		
+		userNameInput=(EditText)findViewById(R.id.user_name_input);
+		loginButton=(Button)findViewById(R.id.login_button);
+	}
+	
+	
+
+	@Override
+	protected void onResume(){
+		super.onResume();
+		loginButton.setOnClickListener(new LoginClick());
+	}
+
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu){
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	class LoginClick implements OnClickListener{
+
+		@Override
+		public void onClick(View v){
+			String inputUserName=userNameInput.getText().toString();
+			//Login / Change Activity
+		}
+		
+	}
+
+}
