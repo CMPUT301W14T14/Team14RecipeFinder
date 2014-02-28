06910d54904d3dfdbda2c095debb782f4612a762
XUPINGI <xuping@ualberta.ca>
2014-02-28 16:22:16 -0700

    added GPS_GEN

---
+++ App/src/controller/GPS_Generator.java
@@ -0,0 +1,53 @@
+package controller;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+
+public class GPS_Generator{
+	private LocationManager lm = null;
+	private Location currentLocation=null;
+	private LocationListener locationListener=null;
+	
+	public GPS_Generator(LocationManager lm){
+		this.lm=lm;
+		this.locationListener=new LocationListener(){
+
+			@Override
+			public void onLocationChanged(Location location){
+				if(location!=null){
+					System.out.println("Location Changed");
+				}
+				else{
+					System.out.println("Error: can not get location.");
+				}
+			}
+
+			@Override
+			public void onProviderDisabled(String provider){}
+
+			@Override
+			public void onProviderEnabled(String provider){}
+
+			@Override
+			public void onStatusChanged(String provider,int status,Bundle extras){}
+			
+		};
+		this.lm.requestLocationUpdates("gps",1000,1,this.locationListener);
+	}
+
+	public Location getCurrentLocation(){
+		this.currentLocation=this.lm.getLastKnownLocation("gps");
+		return this.currentLocation;
+	}
+	
+	public Location getCustomLocation(double latitude,double longitude){
+		Location l=new Location(this.currentLocation);
+		l.setLatitude(latitude);
+		l.setLongitude(longitude);
+		return l;
+	}
+	
+}
--- App/src/controller/TopLevelViewController.java
+++ App/src/controller/TopLevelViewController.java
@@ -14,14 +14,12 @@ import android.content.SharedPreferences;
 
 public class TopLevelViewController{
 	public static final String rootKey="ROOTKEY";
-	private String treePath=null;
 	private CommentList currentList=null;
 	private Gson gson=null;
 	private Context context=null;
 	
 	public TopLevelViewController(Context context){
 		this.gson=new Gson();
-		this.treePath="";
 		this.context=context;
 	}
 	
@@ -29,9 +27,5 @@ public class TopLevelViewController{
 		this.currentList=this.gson.fromJson(db.getString(rootKey,this.gson.toJson(new CommentList(new ArrayList<Comment>()))),CommentList.class);
 		ListViewAdapter adapter=new ListViewAdapter(this.context,this.currentList.getRandomArray());
 		return adapter;
-	}
-	
-	public String getTreePath(){
-		return this.treePath;
 	}
 }
