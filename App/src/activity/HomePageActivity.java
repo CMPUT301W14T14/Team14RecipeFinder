cb6efd68f6e3cbd100b284d7614e7faadca9fd76
jcheng1 <jcheng1@ualberta.ca>
2014-02-27 18:47:47 -0700

    continue on activity

--- App/src/activity/HomePageActivity.java
+++ App/src/activity/HomePageActivity.java
@@ -5,7 +5,10 @@ import com.example.projectapp.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 public class HomePageActivity extends Activity {
@@ -15,12 +18,35 @@ public class HomePageActivity extends Activity {
 	private Button userInfo = null;
 	private Button createNew = null;
 	private ListView greatTopic = null;
+	private ListView topicList = null;
+	private AutoCompleteTextView userInfoText = null;
+	private ImageView picture = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.topicpage);
+		sort = (Button)findViewById(R.id.sort);
+		laterList = (Button)findViewById(R.id.LaterList);
+		userInfo = (Button)findViewById(R.id.userinfo);
+		createNew = (Button)findViewById(R.id.createnew);
+		greatTopic = (ListView)findViewById(R.id.GreatTopic);
+		topicList = (ListView)findViewById(R.id.TopicList);
+		userInfoText = (AutoCompleteTextView)findViewById(R.id.autoCompleteTextView1);
+		picture = (ImageView)findViewById(R.id.picture);
 	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		sort.setOnClickListener(listener);
+		userInfo.setOnClickListener(listener);
+		laterList.setOnClickListener(listener);
+		createNew.setOnClickListener(listener);
+	}
+
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -29,4 +55,30 @@ public class HomePageActivity extends Activity {
 		return true;
 	}
 
+	private View.OnClickListener listener = new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Button button = (Button)v;
+			switch (button.getId()){
+			case R.id.sort:
+				//sort button click
+				
+				break;
+			case R.id.LaterList:
+				//laterlist button click
+				
+				break;
+			case R.id.userinfo:
+				//userinfo button click
+				
+				break;
+			case R.id.createnew:
+				//createnew button click
+				
+				break;
+			}
+		}
+	};
 }
