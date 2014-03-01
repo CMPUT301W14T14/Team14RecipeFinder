f7228bfd0c14bdd26bee6490b14347c7553e6a0c
XUPINGI <xuping@ualberta.ca>
2014-03-01 14:41:45 -0700

    Add CamIntentGen

---
+++ App/src/controller/Camera_Intent_Generator.java
@@ -0,0 +1,23 @@
+package controller;
+
+import java.io.File;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+
+public class Camera_Intent_Generator {
+	
+	public Camera_Intent_Generator(){}
+	
+	public Intent getCamIntent(){
+		Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		String path=Environment.getExternalStorageDirectory().getAbsolutePath()+"/tmp";
+		String date=String.valueOf(System.currentTimeMillis())+".jpeg";
+		File filepic=new File(path+date);
+		Uri imageFileUri=Uri.fromFile(filepic);
+		intent.putExtra(MediaStore.EXTRA_OUTPUT,imageFileUri);
+		return intent;
+	}
+}
