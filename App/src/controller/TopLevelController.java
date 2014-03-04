63f075b2456732fb962d5e8dfaa36dffdbb877c3
XUPINGI <xuping@ualberta.ca>
2014-03-03 20:22:37 -0700

    rewrite the top level Commit Controller

---
+++ App/src/controller/TopLevelController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import network_io.IoStreamHandler;
+import model.Comment;
+import model.CommentList;
+import adapter.ListViewAdapter;
+import android.content.Context;
+
+
+public class TopLevelController{
+	private IoStreamHandler io=null;
+	private Context context=null;
+	
+	public TopLevelController(Context context){
+		this.context=context;
+	}
+	
+	public ListViewAdapter getUpdatedAdapter(){
+		CommentList root=this.io.loadRoot();
+		ListViewAdapter adapter=new ListViewAdapter(this.context,root.getRandomArray());
+		return adapter;
+	}
+	
+	public void commitPublish(Comment comment){
+		CommentList root=this.io.loadRoot();
+		root.addComment(comment);
+		this.io.commitUpdate(root);
+	}
+}
