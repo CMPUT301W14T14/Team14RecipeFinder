c5c03c176de9268e7cddb813c93179c4e374bea6
YiluSu <yilu@ualberta.ca>
2014-03-10 10:45:35 -0600

    Junit tests

--- AppTests/src/com/example/projectapp/test/CommentTest.java
+++ AppTests/src/com/example/projectapp/test/CommentTest.java
@@ -3,12 +3,9 @@
  */
 package com.example.projectapp.test;
 
-import java.util.ArrayList;
-import java.util.Date;
 
-import junit.framework.TestCase;
+import java.util.Date;
 import activity.PublishActivity;
-import android.graphics.Bitmap;
 import android.location.Location;
 import android.test.ActivityInstrumentationTestCase2;
 import model.Comment;
@@ -17,10 +14,10 @@ import model.Comment;
  * @author Yilu
  *
  */
-public class CommentTest extends TestCase {
+public class CommentTest extends ActivityInstrumentationTestCase2<PublishActivity> {
 
 	public CommentTest() {
-		super();
+		super(PublishActivity.class);
 		// TODO Auto-generated constructor stub
 	}
 
@@ -40,75 +37,129 @@ public class CommentTest extends TestCase {
 		assertEquals(comment.getTitle(), "title");
 	}
 	
-	public void setTitle(String newTitle){
-		this.title=newTitle;
-	}
-	
-	//For Text
-	public String getText(){
-		return this.text;
-	}
-	
-	public void setText(String newText){
-		this.text=newText;
-	}
-	
-	//For Location
-	public Location getLocation(){
-		return this.location;
-	}
-	
-	public void setLocation(Location newLocation){
-		this.location=newLocation;
-	}
-	
-	//For picture
-	public Bitmap getPicture(){
-		return this.picture;
-	}
-	
-	//For timePosted
-	public Long getTimePosted(){
-		return this.timePosted;
-	}
-	//For userName
-	public String getUserName(){
-		return this.userName;
-	}
-	
-	//For children
-	public ArrayList<String> getReplies(){
-		return this.replyIdSet;
-	}
-	
-	//For parentId
-	public String getParentId(){
-		return this.parentId;
-	}
-	
-	public void setParent(Comment parent){
-		this.parentId=parent.getId();
-	}
-	
-	public void setParent(String parentId){
-		this.parentId=parentId;
-	}
-	
-	//Methods:
 	/**
-	 * add a comment id of the reply comment to the reply id set.
+	 * Test setTitle method
 	 */
-	
-	public void addReply(Comment comment){
-		this.replyIdSet.add(comment.getId());
+	public void testSetTitle(){
+		Comment comment = new Comment("title","text", null,"userName");
+		comment.setTitle("new title");
+		assertEquals(comment.getTitle(), "new title");
 	}
+	
+	/** 
+	 * Test getText method
+	 */
+	public void testGetText(){
+		Comment comment = new Comment("title","text", null,"userName");
+		assertEquals(comment.getText(), "text");
+	}
+	
 	/**
-	 * add a comment id of the reply comment to the reply id set.
+	 * Test setText method
 	 */
-	
-	public void addReply(String replyId){
-		this.replyIdSet.add(replyId);
+	public void testSetText(String newText){
+		Comment comment = new Comment("title","text", null,"userName");
+		comment.setText("new text");
+		assertEquals(comment.getText(), "new text");
 	}
+	
+	/**
+	 * Test getLocation method
+	 */
+	public void testGetLocation(){
+		Location location = new Location("GPS_PROVIDER");
+		Comment comment = new Comment("title","text", location,"userName");
+		Location lc = comment.getLocation();
+		double lat = lc.getLatitude();
+		double lng = lc.getLongitude();
+		assertTrue(lat==0&&lng==0);
+	}
+	
+	/**
+	 * Test setLocation method
+	 */
+	public void testSetLocation(){
+		Comment comment = new Comment("title","text", null,"userName");
+		Location location = new Location("GPS_PROVIDER");
+		comment.setLocation(location);
+		assertTrue(comment.getLocation()!=null);
+	}
+	
+	/**
+	 * Test getPicture method
+	 */
+	public void testGetPicture(){
+		Comment comment = new Comment("title","text", null, null,"userName");
+		assertEquals(comment.getPicture(), null);
+	}
+	
+	/**
+	 * Test getTimePosted method
+	 */
+	public void testGetTimePosted(){
+		Comment comment = new Comment("title","text", null,"userName");
+		assertTrue(comment.getTimePosted().equals((new Date()).getTime()));
+	}
+	
+	/**
+	 * Test getUserName method
+	 */
+	public void testGetUserName(){
+		Comment comment = new Comment("title","text", null,"userName");
+		assertEquals(comment.getUserName(), "userName");
+	}
+	
+	/**
+	 * Test getReplies method
+	 */
+	public void testGetReplies(){
+		Comment comment = new Comment("title","text", null,"userName");
+		Comment rp1 = new Comment("title1","text1", null,"userName1");
+		Comment rp2 = new Comment("title2","text2", null,"userName2");
+		comment.addReply(rp1);
+		comment.addReply(rp2);
+		assertTrue(comment.getReplies().get(0).equals(rp1)&&comment.getReplies().get(1).equals(rp2));
+	}
+
+	/**
+	 * Test addReply methods
+	 */
+	public void addReply(){
+		Comment comment = new Comment("title","text", null,"userName");
+		Comment rp1 = new Comment("title1","text1", null,"userName1");
+		comment.addReply(rp1);
+		assertTrue(comment.getReplies().get(0).equals(rp1));
+		
+		Comment rp2 = new Comment("title2","text2", null,"userName2");
+		comment.addReply(rp2.getId());
+		assertTrue(comment.getReplies().get(1).equals(rp2));
+	}
+	
+	/**
+	 * Test setParent methods
+	 */
+	public void testSetParent(){
+		Comment comment = new Comment("title","text", null,"userName");
+		Comment rp1 = new Comment("title1","text1", null,"userName1");
+		rp1.setParent(comment);
+		assertTrue(rp1.getParentId().equals(comment.getId()));
+		
+		Comment rp2 = new Comment("title2","text2", null,"userName2");
+		rp2.setParent(comment.getId());
+		assertTrue(rp2.getParentId().equals(comment.getId()));
+	}
+	
+	
+	/**
+	 * Test getParentId method
+	 */
+	public void testGetParentId(){
+		Comment comment = new Comment("title","text", null,"userName");
+		Comment rp1 = new Comment("title1","text1", null,"userName1");
+		comment.addReply(rp1);
+		assertTrue(rp1.getParentId().equals(comment.getId()));
+	}
+
 }
 
 
