5202c8a3b3dfbeb73fe386f314e9f905b527eaf1
XUPINGI <xuping@ualberta.ca>
2014-03-11 19:41:55 -0600

    small changes

--- App/src/comparator/TimeComparator.java
+++ App/src/comparator/TimeComparator.java
@@ -8,7 +8,6 @@ public class TimeComparator implements Comparator<Comment>{
 
 	@Override
 	public int compare(Comment a, Comment b) {
-		// TODO Auto-generated method stub
 		if(a.getTimePosted()>=b.getTimePosted()){
 			return 1;
 		}
