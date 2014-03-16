ac817bf131b02e859fe8eeb4fac4689cc9c6268d
YiluSu <yilu@ualberta.ca>
2014-03-16 15:42:27 -0600

    JUnit tests for Comparator

---
+++ AppTests/src/ca/ualberta/cs/app/test/ComparatorTest.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package ca.ualberta.cs.app.test;
+
+
+import comparator.TimeComparator;
+
+import model.Comment;
+import activity.HomePageActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * 
+ * JUnit test cases for Comparators.
+ * 
+ * @author Yilu
+ *
+ */
+public class ComparatorTest extends ActivityInstrumentationTestCase2<HomePageActivity> {
+
+	/**
+	 * Constructor
+	 */
+	public ComparatorTest() {
+		super(HomePageActivity.class);
+	}
+	
+	/**
+	 * Test the timeComparator function
+	 * Create two comments with an interval of 200 milliseconds. 
+	 * Then, compare the timePosted of them using timeParator method
+	 */
+	public void testTimeComparator() {
+		Comment comment1 = new Comment("title1", "text1", null, "userName1");
+		try {
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Comment comment2 = new Comment("title2", "text2", null, "userName2");
+		TimeComparator timeComparator = new TimeComparator();
+		assertEquals(0, timeComparator.compare(comment1, comment2));
+	}
+	
+	/**
+	 * Test the sortByProximity function
+	 */
+	public void testSortByProximity() {
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 * Test the sortByPicture function
+	 */
+	public void testSortByPicture() {
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 * Test the sortByDate function
+	 */
+	public void testSortByDate() {
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 * Test the sortByScore function
+	 */
+	public void testSortByScore() {
+		fail("Not yet implemented");
+	}
+
+}
