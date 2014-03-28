73d0e359b7de6226d89b72bb7d392e2536624bb3
YiluSu <yilu@ualberta.ca>
2014-03-27 19:58:08 -0600

    AllTopicPageActivity Javadoc

--- App/src/activity/AllTopicPageActivity.java
+++ App/src/activity/AllTopicPageActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.AdapterView.OnItemSelectedListener;
 
@@ -22,21 +23,29 @@ public class AllTopicPageActivity extends Activity implements OnItemSelectedList
 	static String sortByPicture = "Sort By Picture";
 	static String[] sortOption = { sortByDate, sortByMyLocation,
 			sortByOtherLocation, sortByPicture };
+	
+	private ListView listView;
 
+	/**
+	 *  onCreate method. </br>
+	 *  Once the activity is created, first set the content view, and initialize ActionBat and a Spinner for sort options. 
+	 *  Then, load all the topics and adapt to the ListView.
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_all_topic_page);
 
-		//
 		// Initialize View
 		initView();
+		
+		listView = (ListView)findViewById(R.id.topic_list);
 
 	}
 
 	/**
-	 * Initialize View. First, get ActionBar to enable title and disable title.
-	 * Second, initialize spinner for sort options
+	 *  Initialize View. First, get ActionBar to enable title and disable title.
+	 *  Second, initialize spinner for sort options
 	 */
 	private void initView() {
 		// ActionBar
@@ -54,38 +63,9 @@ public class AllTopicPageActivity extends Activity implements OnItemSelectedList
 		spinnerOsversions.setOnItemSelectedListener(this);
 	}
 
-	// /**
-	// * Initialize ActionBar
-	// */
-	// private void initView() {
-	// // get ActionBar
-	// final ActionBar actionBar = getActionBar();
-	// // set navigation mode to tab
-	// actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-	// // enable Logo
-	// actionBar.setDisplayUseLogoEnabled(true);
-	// actionBar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE);
-	// // add Tabs
-	// actionBar.addTab(actionBar
-	// .newTab()
-	// .setText("All Topics")
-	// .setTabListener(
-	// new MyTabListener<FragmentAllTopicPage>(this,
-	// FragmentAllTopicPage.class)));
-	// actionBar.addTab(actionBar
-	// .newTab()
-	// .setText("Favorites")
-	// .setTabListener(
-	// new MyTabListener<FragmentFavoritePage>(this,
-	// FragmentFavoritePage.class)));
-	// actionBar.addTab(actionBar
-	// .newTab()
-	// .setText("My Comments")
-	// .setTabListener(
-	// new MyTabListener<FragmentMyCommentPage>(this,
-	// FragmentMyCommentPage.class)));
-	// }
-
+	/**
+	 * 
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -93,6 +73,9 @@ public class AllTopicPageActivity extends Activity implements OnItemSelectedList
 		return true;
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public boolean onOptionsItemSelected(final MenuItem item) {
 		Intent intent;
@@ -118,8 +101,16 @@ public class AllTopicPageActivity extends Activity implements OnItemSelectedList
 		return super.onOptionsItemSelected(item);
 	}
 
-	public void onItemSelected(AdapterView<?> parent, View view, int position,
-			long id) {
+	/**
+	 * Callback method to be invoked when an item in this view has been selected. 
+	 * This callback is invoked only when the newly selected position is different 
+	 * from the previously selected position or if there was no selected item.
+	 * @param	parent		The AdapterView where the selection happened.
+	 * @param	view		The view within the AdapterView that was clicked
+	 * @param	position	The position of the view in the adapter
+	 * @param	id			The row id of the item that is selected 
+	 */
+	public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 		spinnerOsversions.setSelection(position);
 		String sortSelect = (String) spinnerOsversions.getSelectedItem();
 
@@ -134,6 +125,12 @@ public class AllTopicPageActivity extends Activity implements OnItemSelectedList
 		}
 	}
 
+	/**
+	 * Callback method to be invoked when the selection disappears from this view. 
+	 * The selection can disappear for instance when touch is activated or when the 
+	 * adapter becomes empty.
+	 * @param	arg0	The AdapterView that now contains no selected item. 
+	 */
 	@Override
 	public void onNothingSelected(AdapterView<?> arg0) {
 		// TODO Auto-generated method stub
