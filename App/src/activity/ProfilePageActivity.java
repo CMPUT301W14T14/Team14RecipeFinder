5c7e3bd913506ebd3682c1e442997750051aa939
XUPINGI <xuping@ualberta.ca>
2014-03-30 22:27:09 -0600

    can update personal profile

--- App/src/activity/ProfilePageActivity.java
+++ App/src/activity/ProfilePageActivity.java
@@ -1,13 +1,18 @@
 package activity;
 
 import user.UserNameHandler;
+import model.UserProfile;
 import network_io.ProfileIoHandler;
 
 import com.example.projectapp.R;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.widget.Button;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -20,11 +25,15 @@ public class ProfilePageActivity extends Activity {
 	private EditText biographyInput;
 	private EditText twitterInput;
 	private EditText facebookInput;
-	private Button cancel;
-	private Button commit;
+	private ImageButton cancel;
+	private ImageButton commit;
 	
 	private ProfileIoHandler profileIoHandler=null;
 	private UserNameHandler userNameHandler=null;
+	
+	public static final int OBTAIN_PIC_REQUEST_CODE=252;
+	
+	private Bitmap profilePhoto;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
@@ -38,14 +47,59 @@ public class ProfilePageActivity extends Activity {
 		twitterInput=(EditText)findViewById(R.id.twitter);
 		facebookInput=(EditText)findViewById(R.id.facebook);
 		
-		cancel=(Button)findViewById(R.id.cancel_profile);
-		commit=(Button)findViewById(R.id.commit_profile);
+		cancel=(ImageButton)findViewById(R.id.cancel_profile);
+		commit=(ImageButton)findViewById(R.id.commit_profile);
 		
 		profileIoHandler=new ProfileIoHandler();
 		userNameHandler=new UserNameHandler();
 		
 		profileTitle.setText("User Profile: ");
 		profileIoHandler.loadSpecificProfileForUpdate(userNameHandler.getUserName(this),this, photo, userNameInput, biographyInput, twitterInput, facebookInput);
+		
+		photo.setOnClickListener(new AttachPhotoClick());
+		cancel.setOnClickListener(new CancelClick());
+		commit.setOnClickListener(new UpdateProfileClick());
+	}
+	
+	public void takeAPhoto(){
+		Intent camIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		startActivityForResult(camIntent,OBTAIN_PIC_REQUEST_CODE);
+	}
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == OBTAIN_PIC_REQUEST_CODE && resultCode == RESULT_OK) {
+			profilePhoto = (Bitmap)data.getExtras().get("data");
+			photo.setImageBitmap(profilePhoto);
+		}
+	}
+	
+	class AttachPhotoClick implements OnClickListener{
+
+		@Override
+		public void onClick(View v){
+			takeAPhoto();
+		}
+		
+	}
+	
+	class UpdateProfileClick implements OnClickListener{
+
+		@Override
+		public void onClick(View v){
+			UserProfile newProfile=new UserProfile(userNameInput.getText().toString(),biographyInput.getText().toString(),
+					twitterInput.getText().toString(),facebookInput.getText().toString(),profilePhoto);
+			profileIoHandler.putOrUpdateProfile(newProfile);
+			finish();
+		}
+	}
+	
+	class CancelClick implements OnClickListener{
+
+		@Override
+		public void onClick(View v){
+			finish();
+		}
+		
 	}
 	
 }
