e45c3a37a9a0ba0efcf9c42b1e09eb5d8ee31ffe
XUPINGI <xuping@ualberta.ca>
2014-03-30 21:12:33 -0600

    profile model

--- App/src/user/UserProfile.java
+++ App/src/user/UserProfile.java
@@ -1,6 +1,81 @@
 package user;
 
+import android.graphics.Bitmap;
+
 
 public class UserProfile{
 	
+	private String userName=null;
+	private String biography=null;
+	private String twitter=null;
+	private String facebook=null;
+	private Bitmap photo=null;
+	
+	public UserProfile(String userName,String biography,String twitter,String facebook,Bitmap photo){
+		this.userName=userName;
+		this.biography=biography;
+		this.twitter=twitter;
+		this.facebook=facebook;
+		this.photo=photo;
+	}
+
+	
+	public String getUserName(){
+	
+		return userName;
+	}
+
+	
+	public void setUserName(String userName){
+	
+		this.userName = userName;
+	}
+
+	
+	public String getBiography(){
+	
+		return biography;
+	}
+
+	
+	public void setBiography(String biography){
+	
+		this.biography = biography;
+	}
+
+	
+	public String getTwitter(){
+	
+		return twitter;
+	}
+
+	
+	public void setTwitter(String twitter){
+	
+		this.twitter = twitter;
+	}
+
+	
+	public String getFacebook(){
+	
+		return facebook;
+	}
+
+	
+	public void setFacebook(String facebook){
+	
+		this.facebook = facebook;
+	}
+
+	
+	public Bitmap getPhoto(){
+	
+		return photo;
+	}
+
+	
+	public void setPhoto(Bitmap photo){
+	
+		this.photo = photo;
+	}
 }
