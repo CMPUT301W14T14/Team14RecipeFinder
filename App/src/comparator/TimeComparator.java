a17de39bffeda74cad1cfaeace6f693213dea052
XUPINGI <xuping@ualberta.ca>
2014-04-04 19:17:33 -0600

    ReGenJavaDoc

--- App/src/comparator/TimeComparator.java
+++ App/src/comparator/TimeComparator.java
@@ -4,11 +4,17 @@ import java.util.Comparator;
 
 import model.Comment;
 
+/**
+ * A Comparator which compare and sort 2 Comment objects by time posted.
+ * @author xuping
+ */
 public class TimeComparator implements Comparator<Comment>{
     /**
      * A method which compares the difference between 2 comment's post time while sorting.
-     * @param a a Comment object.
-     * @param b a Comment object.
+     * @param a left hand side Comment object.
+     * @param b right hand side Comment object.
+     * @return -1 if left hand side Comment is posted later than right hand side Comment
+     * 1 otherwise
      */
 	@Override
 	public int compare(Comment a, Comment b) {
